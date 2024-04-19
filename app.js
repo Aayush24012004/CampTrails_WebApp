81938dd39b7de312457337616047dcadb74374f9
Aayush <aayushsingh91255@gmail.com>
2024-04-19 13:54:46 +0530

    Error handling
    > Client-side Form Validation
    >JOI Schema validation
    >JOI Schema validation middleware

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const campground = require("./models/campground");
+const { campgroundSchema } = require("./validationSchema");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
+const catchAsync = require("./utils/catchAsync");
+const ExpressErrors = require("./utils/ExpressErrors");
 mongoose
   .connect("mongodb://127.0.0.1:27017/yelp-camp")
   .then(() => {
@@ -18,45 +21,89 @@ app.set("view engine", "ejs");
 app.engine("ejs", ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressErrors(msg, 400);
+  } else {
+    next();
+  }
+};
 // routing starts here!!!
-app.get("/campgrounds", async (req, res) => {
-  const campgrounds = await campground.find({});
-  res.render("campgrounds/index", { campgrounds });
-});
+app.get(
+  "/campgrounds",
+  catchAsync(async (req, res) => {
+    const campgrounds = await campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
 
-app.get("/campgrounds/new", async (req, res) => {
-  res.render("campgrounds/new");
-});
+app.get(
+  "/campgrounds/new",
+  catchAsync(async (req, res) => {
+    res.render("campgrounds/new");
+  })
+);
 
-app.get("/campgrounds/:id", async (req, res) => {
-  const camp = await campground.findById(req.params.id);
-  res.render("campgrounds/show", { camp });
+app.get(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const camp = await campground.findById(req.params.id);
+    res.render("campgrounds/show", { camp });
+  })
+);
+app.post(
+  "/campgrounds",
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    // if (!req.body.campground)
+    //   throw new ExpressErrors("invalid Campground data", 400);
+    const newCamp = new campground(req.body.campground);
+    await newCamp.save();
+    res.redirect(`/campgrounds/${newCamp._id}`);
+  })
+);
+app.get(
+  "/campgrounds/:id/edit",
+  catchAsync(async (req, res) => {
+    const camp = await campground.findById(req.params.id);
+    res.render("campgrounds/edit", { camp });
+  })
+);
+app.put(
+  "/campgrounds/:id",
+  validateCampground,
+  catchAsync(async (req, res) => {
+    const newCamp = await campground.findByIdAndUpdate(
+      req.params.id,
+      req.body.campground,
+      // In the context of updating a document in Mongoose, ...req.body.campground is generally preferred because it allows you to explicitly specify which fields should be updated while keeping the rest of the document unchanged. This can help prevent unintended modifications to the document and follows the principle of least privilege.
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+    res.redirect(`/campgrounds/${newCamp._id}`);
+  })
+);
+app.delete(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
+app.all("*", (req, res, next) => {
+  next(new ExpressErrors("Page Not Found", 404));
 });
-app.post("/campgrounds", async (req, res) => {
-  const newCamp = new campground(req.body.campground);
-  await newCamp.save();
-  res.redirect(`/campgrounds/${newCamp._id}`);
-});
-app.get("/campgrounds/:id/edit", async (req, res) => {
-  const camp = await campground.findById(req.params.id);
-  res.render("campgrounds/edit", { camp });
-});
-app.put("/campgrounds/:id", async (req, res) => {
-  const newCamp = await campground.findByIdAndUpdate(
-    req.params.id,
-    req.body.campground,
-    // In the context of updating a document in Mongoose, ...req.body.campground is generally preferred because it allows you to explicitly specify which fields should be updated while keeping the rest of the document unchanged. This can help prevent unintended modifications to the document and follows the principle of least privilege.
-    {
-      runValidators: true,
-      new: true,
-    }
-  );
-  res.redirect(`/campgrounds/${newCamp._id}`);
-});
-app.delete("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  await campground.findByIdAndDelete(id);
-  res.redirect("/campgrounds");
+app.use((err, req, res, next) => {
+  const { statuscode = 500 } = err;
+  if (!err.message) {
+    err.message = "Something wwent wrong";
+  }
+  res.status(statuscode).render("error", { err });
 });
 
 app.listen(3000, () => {

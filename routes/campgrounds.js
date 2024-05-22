a1e1e3b37fa7dd6388b9190ece72ee0b93fbed93
Aayush <aayushsingh91255@gmail.com>
2024-05-22 23:31:57 +0530

    Code Cleanup
    rearranged app.js routing code by making separate router folders using express.router, to make it look clean

---
+++ routes/campgrounds.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const router = express.Router();
+const catchAsync = require("../utils/catchAsync");
+const ExpressErrors = require("../utils/ExpressErrors");
+const campground = require("../models/campground");
+const { campgroundSchema } = require("../validationSchema");
+const review = require("../models/review");
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressErrors(msg, 400);
+  } else {
+    next();
+  }
+};
+router.get(
+  "/",
+  catchAsync(async (req, res) => {
+    const campgrounds = await campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
+
+router.get(
+  "/new",
+  catchAsync(async (req, res) => {
+    res.render("campgrounds/new");
+  })
+);
+
+router.get(
+  "/:id",
+  catchAsync(async (req, res) => {
+    const camp = await campground.findById(req.params.id).populate("reviews");
+    res.render("campgrounds/show", { camp });
+  })
+);
+router.post(
+  "/",
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    // if (!req.body.campground)
+    //   throw new ExpressErrors("invalid Campground data", 400);
+    const newCamp = new campground(req.body.campground);
+    await newCamp.save();
+    res.redirect(`/campgrounds/${newCamp._id}`);
+  })
+);
+router.get(
+  "/:id/edit",
+  catchAsync(async (req, res) => {
+    const camp = await campground.findById(req.params.id);
+    res.render("campgrounds/edit", { camp });
+  })
+);
+router.put(
+  "/:id",
+  validateCampground,
+  catchAsync(async (req, res) => {
+    const newCamp = await campground.findByIdAndUpdate(
+      req.params.id,
+      req.body.campground,
+      // In the context of updating a document in Mongoose, ...req.body.campground is generally preferred because it allows you to explicitly specify which fields should be updated while keeping the rest of the document unchanged. This can help prevent unintended modifications to the document and follows the principle of least privilege.
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+    res.redirect(`/campgrounds/${newCamp._id}`);
+  })
+);
+router.delete(
+  "/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
+
+module.exports = router;
---
+++ routes/review.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const campground = require("../models/campground");
+const review = require("../models/review");
+const catchAsync = require("../utils/catchAsync");
+const ExpressErrors = require("../utils/ExpressErrors");
+const { reviewSchema } = require("../validationSchema");
+
+const validateReviews = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressErrors(msg, 400);
+  } else {
+    next();
+  }
+};
+
+router.delete(
+  "/:reviewId",
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } }); //this is pull request used here to pull out the specific object id from the array reviews which contains review ids of all the reviews posted on a specific campground.
+    await review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+  })
+);
+router.post(
+  "/",
+  validateReviews,
+  catchAsync(async (req, res) => {
+    const camp = await campground.findById(req.params.id);
+    const newReview = new review(req.body.review);
+    camp.reviews.push(newReview);
+    await newReview.save();
+    await camp.save();
+    res.redirect(`/campgrounds/${camp._id}`);
+  })
+);
+module.exports = router;

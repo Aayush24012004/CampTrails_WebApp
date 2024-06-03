7f756db659d8d74546b8cc88f411008c38d39c0f
Aayush <aayushsingh91255@gmail.com>
2024-06-03 22:55:52 +0530

    added express-session and express-flash
    used express-session to add session id and expiration duration of the session id. and used express-flash to add flashes messages on creating deleting editing campgrounds and reviews and showing errors

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const methodOverride = require("method-override");
 const ExpressErrors = require("./utils/ExpressErrors");
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/review");
+const session = require("express-session");
+const flash = require("connect-flash");
 mongoose
   .connect("mongodb://127.0.0.1:27017/yelp-camp")
   .then(() => {
@@ -20,8 +22,26 @@ app.set("view engine", "ejs");
 app.engine("ejs", ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-
+app.use(express.static(path.join(__dirname, "public")));
+const sessionConfig = {
+  secret: "harharmahadev",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    // httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 2,
+  },
+};
+app.use(session(sessionConfig));
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 // routing starts here!!!
+
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
 app.all("*", (req, res, next) => {
